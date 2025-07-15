2f0cc837c620b13251078140d15b97013be191fb
Matthew W. <matthew.widdi@gmail.com>
2025-07-15 22:36:42 +1000

    ci: add deployment to github pages
    wip
    
    wip
    
    wip

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	base: "/angularjs-autocomplete/",
 	assetsInclude: ["./src/**/*.html"],
 	resolve: {
 		alias: {
